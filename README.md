e9fa4dc9075e104744b45a0c9655e618ba1359a6
Ryan Abraham <rabraham@uaudio.com>
2019-11-20 14:43:12 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# StatusCage
+
+### Intro
+Use this API to return a Nicolas Cage image in the body of your HTTP responses.
+
+### Website
+[statuscage.com](https://statuscage.com)
+
+### Usage
+```curlrc
+https://ispericoscrowded.com/{status}
+```
+
+### About the Service
+This API is hosted on an AWS CloudFront Distribution and is completely serverless. It is intentionally lean so as to be as performant as possible.
+
+### Want to Contribute?
+Awesome! There are plenty of status codes missing at this time, so if you come across the perfect Nic Meme for a status code, create an issue and send it my way!
+Additionally, this API could use an AWS S3/CloudFront deploy script.
---
+++ js/main.js
@@ -0,0 +1,26 @@
+function renderStatus(code) {
+  return `
+  <li>
+    <a href="/${code}" title="${code} ${statuses[code].message}">
+      <img
+        class="lazyload"
+        src="./img/placeholder.png"
+        data-src="./img/${code}.jpg"
+        alt="${code} ${statuses[code].message}"
+      >
+      <div class="status-info">
+        <h2>${code}</h2>
+        <p>${statuses[code].message}</p>
+      </div>
+    </a>
+  </li>`;
+}
+
+function renderStatusGrid() {
+  const statusGrid = document.getElementById('status-grid');
+  Object.keys(statuses).forEach(code => {
+    if (statuses[code].img) {
+      statusGrid.insertAdjacentHTML('beforeend', renderStatus(code));
+    }
+  });
+}
